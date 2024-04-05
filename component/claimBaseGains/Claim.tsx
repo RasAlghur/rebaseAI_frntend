b5e4712c278dc1df80bb4f38fff1eaa075e966bf
RasAlghur <bakaretoyosi788@gmail.com>
2024-04-05 05:37:45 +0100

    new fixes

--- component/claimBaseGains/Claim.tsx
+++ component/claimBaseGains/Claim.tsx
@@ -1,34 +1,42 @@
 import React, { useState, useEffect } from 'react'
 import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
-import { allowedList } from '../constant/rebaseABI';
 import readTree from "../constant/readTree.json";
 import { useIsMounted } from '../useIsMounted';
-import { useAccount } from 'wagmi';
-
+import { useAccount, useReadContract, useWaitForTransactionReceipt, useWriteContract } from 'wagmi';
+import { BASEG_TEST_CLAIM_ADDR, BASEG_TEST_TOKEN, claimABI } from '../constant/rebaseABI';
 
 const Claim = () => {
-  const [getproofAddress, setGetProofAddress] = useState();
-  const [getproofIndex, setGetProofIndex] = useState();
-  const [getproof, setGetProof] = useState();
+  const [getproofAddress, setGetProofAddress] = useState<string>('');
+  const [getproofIndex, setGetProofIndex] = useState<string>('');
+  const [getProof, setGetProof] = useState<string>('');
+  const [getproofAmount, setGetProofAmount] = useState<string>('');
+  const [getIndex, setGetIndex] = useState<string>('');
+  const [getAmount, setGetAmount] = useState<string>('');
   const { address } = useAccount();
   const mounted = useIsMounted();
+  const { data: hash, isPending, writeContract } = useWriteContract()
 
-  const treeRoot = StandardMerkleTree.of(allowedList, ["address", 'uint256', "uint256"]);
   const treeProof = StandardMerkleTree.load(readTree);
-
-  // (2)
-
   useEffect(() => {
     try {
       for (const [i, v] of treeProof.entries()) {
-        if (v[0] === '0x0000000000000000000000000000000000000001') {
-          // (3)
+        if (v[0] === address) {
           const proof = treeProof.getProof(i);
+          setGetProof(proof);
           setGetProofAddress(proof[0]);
           setGetProofIndex(proof[1]);
-          setGetProof(proof[2]);
-          console.log("Value:", v);
+          setGetProofAmount(proof[2]);
+          setGetIndex(v[1]);
+
+          setGetAmount(v[2]);
+
+          // console.log("Value:", v);
+          // console.log("Index:", v[1]);
+          // console.log("Amount:", v[2]);
           console.log("Proof:", proof);
+          // console.log("Proof Address:", proof[0]);
+          // console.log("Proof Index:", proof[1]);
+          // console.log("Proof Amount:", proof[2]);
         }
       }
     } catch (error) {
@@ -36,41 +44,89 @@ const Claim = () => {
     }
   }, [treeProof, address]);
 
+  const { isLoading: isConfirming, isSuccess: isConfirmed } =
+    useWaitForTransactionReceipt({
+      hash,
+    })
 
+  async function submit() {
+    writeContract({
+      address: BASEG_TEST_CLAIM_ADDR,
+      abi: claimABI,
+      functionName: 'claim',
+      args: [getIndex, getAmount, getProof],
+    });
+    // console.log(getIndex)
+  }
 
   return (
     <div>
+      {/* FOR VERIFICATION, IT WILL BE REMOVED FROM HERE TO LINE 113*/}
+      {/* FOR VERIFICATION, IT WILL BE REMOVED FROM HERE TO LINE 113*/}
+      {/* FOR VERIFICATION, IT WILL BE REMOVED FROM HERE TO LINE 113*/}
       <div>
-        MerkTree:
-        {treeRoot.root}
+        <h1>
+          {mounted
+            ?
+            <p>
+              ProofAddress: {getproofAddress}
+            </p>
+            : null
+          }
+        </h1>
+        <h1>
+          {mounted
+            ?
+            <p>
+              ProofIndex: {getproofIndex}
+            </p>
+            : null
+          }
+        </h1>
+        <h1>
+          {mounted
+            ?
+            <p>
+              Proof: {getproofAmount}
+            </p>
+            : null
+          }
+        </h1>
+        <h1>
+          {mounted && (
+            <p>
+              Index:
+              {getIndex || "0"}
+            </p>
+          )}{" "}
+        </h1>
+        <h1>
+          {mounted && (
+            <p>Amount:
+              {getAmount || "0"}
+            </p>
+          )}{" "}
+        </h1>
       </div>
-
+      {/* CLAIM BUTTON */}
+      {/* CLAIM BUTTON */}
+      {/* CLAIM BUTTON */}
       <div>
-        {mounted
-          ?
-          <p>
-            ProofAddress: {getproofAddress}
-          </p>
-          : null
-        }
-      </div>
-      <div>
-        {mounted
-          ?
-          <p>
-            ProofIndex: {getproofIndex}
-          </p>
-          : null
-        }
-      </div>
-      <div>
-        {mounted
-          ?
-          <p>
-            Proof: {getproof}
-          </p>
-          : null
-        }
+        <button
+          className=' w-16 rounded-full border-4 border-black'
+          disabled={isPending}
+          onClick={submit}
+        >
+          {isPending ? 'Confirming...' : isConfirmed ? 'Transaction confirmed' : 'Claim'}
+        </button>
+        <div>
+          {isConfirming && <div>Waiting for confirmation...</div>}
+          {/* {isConfirmed && <div>Transaction confirmed.</div>} */}
+          {/* https://testnet.bscscan.com/tx/0xba82d994a24207cd23d1ad473690820375e41a0f5b3a79fd8e840e24ade2e4de */}
+          {hash && <div>
+            <a href="https://testnet.bscscan.com/tx/$hash"></a>
+            Transaction Hash: {hash}</div>}
+        </div>
       </div>
 
       {/* {tree.} */}
--- component/constant/rebaseABI.tsx
+++ component/constant/rebaseABI.tsx
@@ -1728,16 +1728,10 @@ export const swapABI = [
   },
 ];
 
-export const allowedList = [
-  ["0x0000000000000000000000000000000000000001", "0", "10000000000000000000"],
-  ["0x0000000000000000000000000000000000000002", "1", "20000000000000000000"],
-  ["0x0000000000000000000000000000000000000003", "2", "30000000000000000000"],
-  ["0x0000000000000000000000000000000000000004", "3", "40000000000000000000"],
-  ["0x0000000000000000000000000000000000000005", "4", "50000000000000000000"],
-  ["0x0000000000000000000000000000000000000006", "5", "60000000000000000000"],
-  ["0x0000000000000000000000000000000000000007", "6", "70000000000000000000"],
-  ["0x0000000000000000000000000000000000000008", "7", "80000000000000000000"],
+export const claimABI = [
+  { "inputs": [{ "internalType": "address", "name": "initalOwner", "type": "address" }, { "internalType": "address", "name": "_claimedToken", "type": "address" }, { "internalType": "address", "name": "_requiredToken", "type": "address" }, { "internalType": "bytes32", "name": "_merkleRoot", "type": "bytes32" }], "stateMutability": "nonpayable", "type": "constructor" }, { "inputs": [], "name": "AlreadyClaimed", "type": "error" }, { "inputs": [], "name": "InvalidProof", "type": "error" }, { "inputs": [{ "internalType": "address", "name": "owner", "type": "address" }], "name": "OwnableInvalidOwner", "type": "error" }, { "inputs": [{ "internalType": "address", "name": "account", "type": "address" }], "name": "OwnableUnauthorizedAccount", "type": "error" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "previousOwner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "newOwner", "type": "address" }], "name": "OwnershipTransferred", "type": "event" }, { "inputs": [{ "internalType": "uint256", "name": "index", "type": "uint256" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }, { "internalType": "bytes32[]", "name": "merkleProof", "type": "bytes32[]" }], "name": "claim", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "claimActivated", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "claimEnd", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "claimStart", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "claimedToken", "outputs": [{ "internalType": "contract IERC20", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "merkleRoot", "outputs": [{ "internalType": "bytes32", "name": "", "type": "bytes32" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "owner", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "renounceOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "requiredToken", "outputs": [{ "internalType": "contract IERC20", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "startClaim", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "newOwner", "type": "address" }], "name": "transferOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "stateMutability": "payable", "type": "receive" }
 ];
+
 
 //BASE 
 export const weth = "0x4200000000000000000000000000000000000006";
@@ -1749,3 +1743,8 @@ export const DEADWALLET = "0x000000000000000000000000000000000000dEaD";
 // NODE
 // Base
 export const BASE_MAINNET_RPC = 'https://base-mainnet.g.alchemy.com/v2/GvkpnU0AGiGdDRUbWoiwrTgDhy2oSWTF';
+
+
+// TEST ON BSC
+export const BASEG_TEST_TOKEN = "0x9aDD56E3B04b7882672251F8AE7c7d270e55F5a2";
+export const BASEG_TEST_CLAIM_ADDR = "0xa003f56A816f89E941CcE31293627f77A7A828e9";
