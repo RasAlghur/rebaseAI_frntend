c42235073cb46a0637f964a25822f2a3580cd5cf
RasAlghur <bakaretoyosi788@gmail.com>
2024-04-05 14:36:15 +0100

    newlu fixes

--- component/claimBaseGains/Claim.tsx
+++ component/claimBaseGains/Claim.tsx
@@ -1,48 +1,47 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
 import readTree from "../constant/readTree.json";
 import { useIsMounted } from '../useIsMounted';
 import { useAccount, useReadContract, useWaitForTransactionReceipt, useWriteContract } from 'wagmi';
-import { BASEG_TEST_CLAIM_ADDR, BASEG_TEST_TOKEN, claimABI } from '../constant/rebaseABI';
+import { BASEG_TEST_CLAIM_ADDR, claimABI } from '../constant/rebaseABI';
 
 const Claim = () => {
-  const [getproofAddress, setGetProofAddress] = useState<string>('');
-  const [getproofIndex, setGetProofIndex] = useState<string>('');
-  const [getProof, setGetProof] = useState<string>('');
-  const [getproofAmount, setGetProofAmount] = useState<string>('');
+  const [getAddress, setGetAddress] = useState<string>('');
+  const [getProof, setGetProof] = useState<string[]>([]);
   const [getIndex, setGetIndex] = useState<string>('');
   const [getAmount, setGetAmount] = useState<string>('');
   const { address } = useAccount();
   const mounted = useIsMounted();
-  const { data: hash, isPending, writeContract } = useWriteContract()
+  const { data: hash, isPending, writeContract } = useWriteContract();
 
-  const treeProof = StandardMerkleTree.load(readTree);
+  const jsonString = JSON.stringify(readTree);
+
   useEffect(() => {
     try {
-      for (const [i, v] of treeProof.entries()) {
+
+      const tree = StandardMerkleTree.load(JSON.parse(jsonString));
+
+      for (const [i, v] of tree.entries()) {
         if (v[0] === address) {
-          const proof = treeProof.getProof(i);
+          const proof = tree.getProof(i);
           setGetProof(proof);
-          setGetProofAddress(proof[0]);
-          setGetProofIndex(proof[1]);
-          setGetProofAmount(proof[2]);
+          setGetAddress(v[0]);
           setGetIndex(v[1]);
-
           setGetAmount(v[2]);
-
-          // console.log("Value:", v);
-          // console.log("Index:", v[1]);
-          // console.log("Amount:", v[2]);
-          console.log("Proof:", proof);
-          // console.log("Proof Address:", proof[0]);
-          // console.log("Proof Index:", proof[1]);
-          // console.log("Proof Amount:", proof[2]);
+          break; // Once a match is found, exit the loop
         }
       }
     } catch (error) {
-      console.log('No data')
+      console.log('Error loading Merkle tree:', error);
     }
-  }, [treeProof, address]);
+  }, [address, jsonString]);
+
+  const { data: verifyClaimed } = useReadContract({
+    abi: claimABI,
+    address: BASEG_TEST_CLAIM_ADDR,
+    functionName: "airdropped",
+    args: [address],
+  });
 
   const { isLoading: isConfirming, isSuccess: isConfirmed } =
     useWaitForTransactionReceipt({
@@ -50,89 +49,50 @@ const Claim = () => {
     })
 
   async function submit() {
-    writeContract({
-      address: BASEG_TEST_CLAIM_ADDR,
-      abi: claimABI,
-      functionName: 'claim',
-      args: [getIndex, getAmount, getProof],
-    });
-    // console.log(getIndex)
+    if (verifyClaimed == true) {
+      return <div>
+        Already Claimed
+      </div>
+    } else {
+      writeContract({
+        address: BASEG_TEST_CLAIM_ADDR,
+        abi: claimABI,
+        functionName: 'claim',
+        args: [getIndex, getAmount, getProof],
+      });
+    }
   }
 
   return (
     <div>
-      {/* FOR VERIFICATION, IT WILL BE REMOVED FROM HERE TO LINE 113*/}
-      {/* FOR VERIFICATION, IT WILL BE REMOVED FROM HERE TO LINE 113*/}
-      {/* FOR VERIFICATION, IT WILL BE REMOVED FROM HERE TO LINE 113*/}
       <div>
         <h1>
-          {mounted
-            ?
-            <p>
-              ProofAddress: {getproofAddress}
-            </p>
-            : null
-          }
+          {mounted && getAddress && address ? <p>
+            Verify Eligibility:
+            {getAddress ? 'Congratulation, You are Eligible to Claim Airdrop' : 'Ops, You are not Eligible to Claim Airdrop'}
+          </p> : 'Ops, You are not Eligible to Claim Airdrop'}
         </h1>
         <h1>
-          {mounted
-            ?
-            <p>
-              ProofIndex: {getproofIndex}
-            </p>
-            : null
-          }
+          {mounted && getAmount ? <p>
+            Claim Amount:
+            {getAddress ? getAmount : '0'}
+          </p> : '0'}
         </h1>
-        <h1>
-          {mounted
-            ?
-            <p>
-              Proof: {getproofAmount}
-            </p>
-            : null
-          }
-        </h1>
-        <h1>
-          {mounted && (
-            <p>
-              Index:
-              {getIndex || "0"}
-            </p>
-          )}{" "}
-        </h1>
-        <h1>
-          {mounted && (
-            <p>Amount:
-              {getAmount || "0"}
-            </p>
-          )}{" "}
-        </h1>
-      </div>
-      {/* CLAIM BUTTON */}
-      {/* CLAIM BUTTON */}
-      {/* CLAIM BUTTON */}
-      <div>
         <button
           className=' w-16 rounded-full border-4 border-black'
           disabled={isPending}
           onClick={submit}
         >
-          {isPending ? 'Confirming...' : isConfirmed ? 'Transaction confirmed' : 'Claim'}
+          {mounted && getAddress && address ? <p>
+            {getAddress ? isPending ? 'Confirming...' : isConfirming ? 'Waiting for confirmation...' : isConfirmed ? 'Claimed' : verifyClaimed === true ? 'Already Claimed' : 'Claim' : 'Not Eligible'}
+          </p> : 'Not Eligible'}
         </button>
         <div>
-          {isConfirming && <div>Waiting for confirmation...</div>}
-          {/* {isConfirmed && <div>Transaction confirmed.</div>} */}
-          {/* https://testnet.bscscan.com/tx/0xba82d994a24207cd23d1ad473690820375e41a0f5b3a79fd8e840e24ade2e4de */}
           {hash && <div>
-            <a href="https://testnet.bscscan.com/tx/$hash"></a>
-            Transaction Hash: {hash}</div>}
+            <a href={`https://testnet.bscscan.com/tx/${hash}`}>Claimed Successfully: view  Txn at {hash}</a>
+          </div>}
         </div>
       </div>
-
-      {/* {tree.} */}
-      {/* 0xd4dee0beab2d53f2cc83e567171bd2820e49898130a22622b10ead383e90bd77 */}
-      {/* 0xd4dee0beab2d53f2cc83e567171bd2820e49898130a22622b10ead383e90bd77 */}
-      {/* 0xa363ce445148603408e6b99e5f58271a80b194bfce04d7270672f0ac98e086f5 */}
     </div>
   )
 }
