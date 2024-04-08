120ec4a6cb7766341420c2fc6fd7925bb8b2f48b
RasAlghur <bakaretoyosi788@gmail.com>
2024-04-08 20:59:29 +0100

    we

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import type { NextPage } from "next";
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import { useAccount, useChainId } from "wagmi";
 import Dashboard from "../component/dashboard/Dashboard";
 
